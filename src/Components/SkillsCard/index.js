ee1756eadb0c6033293ad9a3584f6f1f9a1fad0b
Shivangam Soni <shivangamsoni99@gmail.com>
2021-12-03 20:13:45 +0530

    Edit: Portfolio & Skills

--- src/Components/SkillsCard/index.js
+++ src/Components/SkillsCard/index.js
@@ -1,64 +1,64 @@
 import style from "./style.module.css";
 
 const Skills = () => {
+  const skillsList = [
+    {
+      id: 1,
+      label: "HTML",
+      image: require("../../Assets/icons/html5.svg").default,
+    },
+    {
+      id: 2,
+      label: "CSS",
+      image: require("../../Assets/icons/css3.svg").default,
+    },
+    {
+      id: 3,
+      label: "JavaScript",
+      image: require("../../Assets/icons/javascript.svg").default,
+    },
+    {
+      id: 4,
+      label: "REACT",
+      image: require("../../Assets/icons/react.svg").default,
+    },
+    {
+      id: 5,
+      label: "REDUX",
+      image: require("../../Assets/icons/redux.svg").default,
+    },
+    {
+      id: 6,
+      label: "SASS",
+      image: require("../../Assets/icons/sass.svg").default,
+    },
+    {
+      id: 7,
+      label: "Git",
+      image: require("../../Assets/icons/git.svg").default,
+    },
+    {
+      id: 8,
+      label: "GitHub",
+      image: require("../../Assets/icons/github.svg").default,
+    },
+    {
+      id: 9,
+      label: "Linux Basics",
+      image: require("../../Assets/icons/linux.svg").default,
+    },
+  ];
+
   return (
     <div className={style.container}>
       <h3 className={style.title}>Languages / Libraries / Technologies</h3>
       <div className={style.skills}>
-        <div className={style.skill}>
-          <img alt="HTML" src={require("../../Assets/icons/html5.svg").default} />
-          <span>HTML</span>
-        </div>
-
-        <div className={style.skill}>
-          <img alt="CSS" src={require("../../Assets/icons/css3.svg").default} />
-          <span>CSS</span>
-        </div>
-
-        <div className={style.skill}>
-          <img alt="JavaScript" src={require("../../Assets/icons/javascript.svg").default} />
-          <span>JavaScript</span>
-        </div>
-
-        <div className={style.skill}>
-          <img alt="REACT" src={require("../../Assets/icons/react.svg").default} />
-          <span>REACT</span>
-        </div>
-
-        <div className={style.skill}>
-          <img alt="Git" src={require("../../Assets/icons/git.svg").default} />
-          <span>Git</span>
-        </div>
-
-        <div className={style.skill}>
-          <img alt="SASS" src={require("../../Assets/icons/sass.svg").default} />
-          <span>SASS</span>
-        </div>
-
-        <div className={style.skill}>
-          <img alt="GitHub" src={require("../../Assets/icons/github.svg").default} />
-          <span>GitHub</span>
-        </div>
-
-        <div className={style.skill}>
-          <img alt="Linux" src={require("../../Assets/icons/linux.svg").default} />
-          <span>Linux</span>
-        </div>
-
-        {/* <div className={style.skill}>
-          <img alt="Googling" src={require("../../Assets/icons/google.svg").default} />
-          <span>Googling</span>
-        </div> */}
-
-        <div className={style.skill}>
-          <img alt="AWS" src={require("../../Assets/icons/aws.svg").default} />
-          <span>AWS</span>
-        </div>
-
-        <div className={style.skill}>
-          <img alt="Redux" src={require("../../Assets/icons/redux.svg").default} />
-          <span>Redux</span>
-        </div>
+        {skillsList.map((skill) => (
+          <div key={skill.id} className={style.skill}>
+            <img alt="HTML" src={skill.image} />
+            <span>{skill.label}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/Container/Portfolio/index.js
+++ src/Container/Portfolio/index.js
@@ -5,6 +5,20 @@ import style from "./style.module.css";
 const Projects = () => {
   const projectData = [
     {
+      id: 7,
+      title: "Blog Web Site Front-End",
+      desc: "A Blog App Created created using REACT, react-router & Context API",
+      image: require("../../Assets/Projects/BlogReact.png").default,
+      live: "https://blog-react-shivangam-soni.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "YouTube Video Viewer",
+      desc: "YouTube Video Viewer created using React, Redux, React-Redux, Redux-Thunk, Axios & Google YouTube Data APIv3",
+      image: require("../../Assets/Projects/YouTube.png").default,
+      live: "https://shivi-youtube.netlify.app/",
+    },
+    {
       id: 1,
       title: "Todo React",
       desc: "Todo App Created Using REACT, HTML & CSS.",
@@ -12,11 +26,11 @@ const Projects = () => {
       live: "https://shivi-todo-react.netlify.app/",
     },
     {
-      id: 5,
-      title: "Personal Portfolio Webpage",
-      desc: "A Personal Portfolio Web Page made using HTML & CSS. I made this project as part of Free Code Camp Front-End Developer Certification",
-      image: require("../../Assets/Projects/PersonalPortfolioWebPage.png").default,
-      live: "https://shivangamsoni.github.io/FreeCodeCamp/Responsive-Web-Design/Personal-Portfolio-Webpage/",
+      id: 2,
+      title: "Todo JS",
+      desc: "Todo App Created using HTML, CSS & JS",
+      image: require("../../Assets/Projects/TodoApp.png").default,
+      live: "https://shivi-todo-app.netlify.app/",
     },
     {
       id: 3,
@@ -26,13 +40,6 @@ const Projects = () => {
       live: "https://shivi-clock-timer.netlify.app/",
     },
     {
-      id: 6,
-      title: "Product Landing Page",
-      desc: "A Product Landing Web Page made using HTML & CSS. I made this project as part of Free Code Camp Front-End Developer Certification",
-      image: require("../../Assets/Projects/ProductLandingPage.png").default,
-      live: "https://shivangamsoni.github.io/FreeCodeCamp/Responsive-Web-Design/Product-Landing-Page/",
-    },
-    {
       id: 4,
       title: "Tribute Page",
       desc: "Tribute Page for Robert Downey Jr.(RDJ) created using HTML & CSS. I made this project as part of Free Code Camp Front-End Developer Certification",
@@ -40,11 +47,18 @@ const Projects = () => {
       live: "https://shivangamsoni.github.io/FreeCodeCamp/Responsive-Web-Design/Tribute-Page/",
     },
     {
-      id: 2,
-      title: "Todo JS",
-      desc: "Todo App Created using HTML, CSS & JS",
-      image: require("../../Assets/Projects/TodoApp.png").default,
-      live: "https://shivi-todo-app.netlify.app/",
+      id: 5,
+      title: "Personal Portfolio Webpage",
+      desc: "A Personal Portfolio Web Page made using HTML & CSS. I made this project as part of Free Code Camp Front-End Developer Certification",
+      image: require("../../Assets/Projects/PersonalPortfolioWebPage.png").default,
+      live: "https://shivangamsoni.github.io/FreeCodeCamp/Responsive-Web-Design/Personal-Portfolio-Webpage/",
+    },
+    {
+      id: 6,
+      title: "Product Landing Page",
+      desc: "A Product Landing Web Page made using HTML & CSS. I made this project as part of Free Code Camp Front-End Developer Certification",
+      image: require("../../Assets/Projects/ProductLandingPage.png").default,
+      live: "https://shivangamsoni.github.io/FreeCodeCamp/Responsive-Web-Design/Product-Landing-Page/",
     },
   ];
 
